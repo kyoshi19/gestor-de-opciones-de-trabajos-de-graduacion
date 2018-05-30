678ad05ffd0b8f867c8650bb41b06303a5b6c3a1
Jorge Girón <32642460+kyoshi19@users.noreply.github.com>
2018-05-30 16:30:22 -0500

    Add files via upload

--- SiGeTraG/php/insertUser.php
+++ SiGeTraG/php/insertUser.php
@@ -13,10 +13,18 @@ $lname = next($data);		//Optiene Siguiente elemento
 $document = next($data);
 $pass = next($data);
 
-$result = $conn->query("INSERT INTO utp_users (us_fname, us_lname, us_doc_num, us_pass) 
-VALUES('$fname', '$lname', '$document', '$pass' );");		//ejecucion de query contra la base de datos
+$sql="INSERT INTO utp_users (us_fname, us_lname, us_doc_num, us_pass)
+VALUES('$fname', '$lname', '$document', '$pass' );";		//ejecucion de query contra la base de datos
 
-$outp ='{"records":['.$result.']}';  	//Convierte despuesta a formato json
+
+$result = $conn->query($sql);
+
+if ($result === TRUE) {
+  $outp ='{"records":['.$result.']}';  	//Convierte despuesta a formato json
+}else{
+  $error = print_r($conn->error, TRUE);
+  $outp = '{"error":"'.$error.'"}';
+}
 
 $conn->close();
 
