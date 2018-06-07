4183c8c9fbdfd6f74fb585270529a18e2352d02d
Jorge Girón <32642460+kyoshi19@users.noreply.github.com>
2018-06-07 16:23:03 -0500

    Add files via upload

---
+++ SiGeTraG/php/selectCatalogs.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "root", "root", "utp_tesis");
+
+$resultCatalogs = $conn->query("SELECT cat_name AS 'name', cat_code AS 'code' FROM catalogs;");
+
+$outp = "";
+$res = "";
+
+while($rs = $resultCatalogs->fetch_array(MYSQLI_ASSOC)) {
+	if ($outp != "") {$outp .= ",";}
+	$outp .= '"'.$rs["name"] . '":[';
+
+	$resultItems = $conn->query("SELECT cati_code AS 'id', cati_name AS 'name' FROM catalogs_items WHERE cat_code = '".$rs["code"]."';");
+	while($rrs = $resultItems->fetch_array(MYSQLI_ASSOC)){
+		if ($res != "") {$res .= ",";}
+		$res .= '{"id":"'.$rrs["id"].'","name":"'.$rrs["name"].'"}';	  
+	}
+	$outp .= $res .']';
+	$res = "";
+
+}
+$outp ='{'.$outp.'}';
+$conn->close();
+echo($outp);
+?>
