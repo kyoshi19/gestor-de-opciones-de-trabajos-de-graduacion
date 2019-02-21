5e3cb91ab6c54f334a165d74a0b777847a3cc557
Jorge Girón <32642460+kyoshi19@users.noreply.github.com>
2019-02-21 11:48:07 -0500

    Actualizando SP y BackUp

--- docs/scrips/CreateTesisDataBase.sql
+++ docs/scrips/CreateTesisDataBase.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `utp_tesis` /*!40100 DEFAULT CHARACTER SET utf8 C
 USE `utp_tesis`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: localhost    Database: utp_tesis
+-- Host: 127.0.0.1    Database: utp_tesis
 -- ------------------------------------------------------
 -- Server version	5.7.12-log
 
@@ -16,6 +16,34 @@ USE `utp_tesis`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `candidates`
+--
+
+DROP TABLE IF EXISTS `candidates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `candidates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `work_code` int(11) NOT NULL,
+  `faculty` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `career` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`,`work_code`),
+  KEY `fk_work_code_idx` (`work_code`),
+  CONSTRAINT `fk_work_code` FOREIGN KEY (`work_code`) REFERENCES `graduation_works` (`work_code`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `candidates`
+--
+
+LOCK TABLES `candidates` WRITE;
+/*!40000 ALTER TABLE `candidates` DISABLE KEYS */;
+INSERT INTO `candidates` VALUES (1,52,'FCT','LCEB'),(9,56,'FISC','IS'),(10,57,'FISC','LDS'),(11,57,'FISC','LRI'),(12,57,'FISC','IS'),(13,58,'FISC','ISC'),(14,58,'FIC','IG'),(15,59,'FISC','IS'),(24,60,'FISC','LIAE');
+/*!40000 ALTER TABLE `candidates` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `catalogs`
@@ -38,7 +66,7 @@ CREATE TABLE `catalogs` (
 
 LOCK TABLES `catalogs` WRITE;
 /*!40000 ALTER TABLE `catalogs` DISABLE KEYS */;
-INSERT INTO `catalogs` VALUES ('graduationWorkTypes','GW'),('utpFaculties','UF'),('utpRegionalCenters','URC');
+INSERT INTO `catalogs` VALUES ('graduationWorkTypes','GW'),('utpCareers','UC'),('utpFaculties','UF'),('utpRegionalCenters','URC');
 /*!40000 ALTER TABLE `catalogs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +93,7 @@ CREATE TABLE `catalogs_items` (
 
 LOCK TABLES `catalogs_items` WRITE;
 /*!40000 ALTER TABLE `catalogs_items` DISABLE KEYS */;
-INSERT INTO `catalogs_items` VALUES ('GW','Práctica Profesional','P',''),('GW','Trabajo teórico','T',''),('GW','Trabajo Teórico-Práctico','TP',''),('UF','Facultad de Ciencias y Tecnología','FCT',''),('UF','Facultad de Ingeniería Civil','FIC',''),('UF','Facultad de Ingeniería Eléctrica','FIE',''),('UF','Facultad de Ingeniería Industrial','FII',''),('UF','Facultad de Ingeniería Mecánica','FIM',''),('UF','Facultad de Ingeniería de Sistemas Computacionales','FISC',''),('URC','Campus “Dr. Víctor Levi Sasso”','PC',''),('URC','Sede de Howard','PH',''),('URC','Sede de Tocumen','PT',''),('URC','Centro Regional de Azuero','RA',''),('URC','Centro Regional de Bocas del Toro','RB',''),('URC','Centro Regional de Chiriquí','RCH',''),('URC','Centro Regional de Colón','RCL',''),('URC','Centro Regional de Coclé','RCO',''),('URC','Centro Regional de Panamá Oeste','RPO',''),('URC','Centro Regional de Veraguas','RV','');
+INSERT INTO `catalogs_items` VALUES ('GW','Práctica Profesional','P',''),('GW','Trabajo teórico','T',''),('GW','Trabajo Teórico-Práctico','TP',''),('UC','Licenciatura en Ingeniería en Alimentos','IA','FCT'),('UC','Licenciatura en Ingeniería Ambiental','IAM','FIC'),('UC','Licenciatura en Ingeniería Aeronáutica','IAN','FIM'),('UC','Licenciatura en Ingeniería Civil','IC','FIC'),('UC','Licenciatura en Ingeniería de Control y Automatización','ICA','FIE'),('UC','Licenciatura en Ingeniería Eléctrica','IE','FIE'),('UC','Licenciatura en Ingeniería de Energía y Ambiente','IEA','FIM'),('UC','Licenciatura en Ingeniería Eléctrica y Electrónica','IEE','FIE'),('UC','Licenciatura en Ingeniería Electromecánica','IEM','FIE'),('UC','Licenciatura en Ingeniería Electrónica y Telecomunicaciones','IET','FIE'),('UC','Licenciatura en Ingeniería Forestal','IF','FCT'),('UC','Licenciatura en Ingeniería Geomática','IG','FIC'),('UC','Licenciatura en Ingeniería Geológica','IGE','FIC'),('UC','Licenciatura en Ingeniería Industrial','II','FII'),('UC','Licenciatura en Ingeniería Logística y Cadena de Suministro','ILCS','FII'),('UC','Licenciatura en Ingeniería Mecánica','IM','FIM'),('UC','Licenciatura en Ingeniería Mecánica Industrial','IMI','FII'),('UC','Licenciatura en Ingeniería de Mantenimiento','IMM','FIM'),('UC','Licenciatura en Ingeniería Marítima Portuaria','IMP','FIC'),('UC','Licenciatura en Ingeniería Naval','IN','FIM'),('UC','Licenciatura en Ingeniería de Software','IS','FISC'),('UC','Licenciatura en Ingeniería de Sistemas y Computación','ISC','FISC'),('UC','Licenciatura en Ingeniería de Sistemas de Información','ISI','FISC'),('UC','Licenciatura en Ingeniería en Telecomunicaciones','IT','FIE'),('UC','Licenciatura en Administración de Aviación (Piloto)','LAA','FIM'),('UC','Licenciatura en Administración de Aviación con Opción a Vuelo (Piloto)','LAAOV','FIM'),('UC','Licenciatura en Comunicación Ejecutiva Bilingüe','LCEB','FCT'),('UC','Licenciatura En Dibujo Automatizado','LDA','FIC'),('UC','Licenciatura en Desarrollo de Software','LDS','FISC'),('UC','Licenciatura en Edificaciones','LE','FIC'),('UC','Licenciatura en Electrónica y Sistemas de Comunicación','LESC','FIE'),('UC','Licenciatura en Gestión Administrativa','LGA','FII'),('UC','Licenciatura en Gestión de la Producción Industrial','LGPI','FII'),('UC','Licenciatura en Informática Aplicada a la Educación','LIAE','FISC'),('UC','Licenciatura en Logística y Transporte Multimodal','LLTM','FII'),('UC','Licenciatura en Mecánica Automotriz','LMA','FIM'),('UC','Licenciatura en Mercadeo y Comercio Internacional','LMCI','FII'),('UC','Licenciatura en Mecánica Industrial','LMI','FIM'),('UC','Licenciatura en Mercadeo y Negocios Internacionales','LMNI','FII'),('UC','Licenciatura en Operaciones Marítimas y Portuarias','LOMP','FIC'),('UC','Licenciatura en Refrigeración y Aire Acondicionado','LRAA','FIM'),('UC','Licenciatura en Recursos Humanos y Gestión de la Productividad','LRHGP','FII'),('UC','Licenciatura en Redes Informáticas','LRI','FISC'),('UC','Licenciatura en Soldadura','LS','FIM'),('UC','Licenciatura en Saneamiento y Ambiente','LSA','FIC'),('UC','Licenciatura en Sistemas Eléctricos y Automatización','LSEA','FIE'),('UC','Licenciatura en Topografía','LT','FIC'),('UC','Técnico en Autotrónica','TAT','FIE'),('UC','Técnico en Despacho de Vuelo','TDV','FIM'),('UC','Técnico en Electrónica Biomédica','TEB','FIE'),('UC','Técnico en Informática para la Gestión Empresarial','TIGE','FISC'),('UC','Técnico en Ingeniería de Mantenimiento de Aeronaves con especialización en Motores y Fuselaje','TIMAEMF','FIM'),('UC','Técnico en Sistemas Eléctricos','TSE','FIE'),('UC','Técnico en Telecomunicaciones','TTC','FIE'),('UF','Facultad de Ciencias y Tecnología','FCT',''),('UF','Facultad de Ingeniería Civil','FIC',''),('UF','Facultad de Ingeniería Eléctrica','FIE',''),('UF','Facultad de Ingeniería Industrial','FII',''),('UF','Facultad de Ingeniería Mecánica','FIM',''),('UF','Facultad de Ingeniería de Sistemas Computacionales','FISC',''),('URC','Campus “Dr. Víctor Levi Sasso”','PC',''),('URC','Sede de Howard','PH',''),('URC','Sede de Tocumen','PT',''),('URC','Centro Regional de Azuero','RA',''),('URC','Centro Regional de Bocas del Toro','RB',''),('URC','Centro Regional de Chiriquí','RCH',''),('URC','Centro Regional de Colón','RCL',''),('URC','Centro Regional de Coclé','RCO',''),('URC','Centro Regional de Panamá Oeste','RPO',''),('URC','Centro Regional de Veraguas','RV','');
 /*!40000 ALTER TABLE `catalogs_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,6 +108,7 @@ CREATE TABLE `graduation_works` (
   `work_code` int(11) NOT NULL AUTO_INCREMENT,
   `work_type` char(2) CHARACTER SET utf8 NOT NULL,
   `w_title` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `work_desc` varchar(300) CHARACTER SET utf8 NOT NULL,
   `advisor` varchar(45) CHARACTER SET utf8 NOT NULL,
   `faculty` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
   `reg_center` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
@@ -88,7 +117,7 @@ CREATE TABLE `graduation_works` (
   PRIMARY KEY (`work_code`),
   KEY `advisor_idx` (`advisor`),
   CONSTRAINT `advisor` FOREIGN KEY (`advisor`) REFERENCES `utp_users` (`us_doc_num`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +126,7 @@ CREATE TABLE `graduation_works` (
 
 LOCK TABLES `graduation_works` WRITE;
 /*!40000 ALTER TABLE `graduation_works` DISABLE KEYS */;
-INSERT INTO `graduation_works` VALUES (13,'P','Impresión automática de actas en el Tribunal Electoral para las eleciones','5-482-691','FISC','PC','2018-07-13',2),(14,'TP','Manual de procedimiento para el diseño de sistemas de fontanería','1-123-11','FIC','PC','2018-04-24',1),(15,'TP','Diseño eléctrico realizado en la compañía ingeniería atlántico','8-475-1024','FIE','PC','2017-10-08',2),(16,'P','Reconocimiento, evaluación y manejo de equipos especiales de fuerza en las centrales telefónicas.','PE-14-5298','FII','PC','2017-06-10',3),(17,'TP','Sistema de gestión de la información del departamento de Bienestar Estudiantil de la Universidad Tecnológica de Panamá Centro Regional de Chiriquí','5-482-691','FIE','PC','2018-02-10',1),(18,'P','Práctica profesional en Ricardo Perez S.A. \'Ahorro energético y mejoras en la calidad de la energía\'','8-475-1024','FIE','PC','2018-02-10',2),(20,'T','Diseño y construcción del sistema didáctico SD-88 basado en el microprocesador INTEL8088','PE-14-5298','FISC','PC','2018-07-26',4),(21,'T','Nuevo método computacional para el estudio de flujo de potenciaaa','8-475-1024','FISC','Pc','2018-07-26',2),(22,'P','Mantenimiento preventivo en los sistemas de transmisión digital por cables de fibras óptica y microondas.','5-482-691','FISC','PC','2018-07-26',3),(23,'P','Implementación de un sistema de información para la logística de materiales de una empresa industrial','1-123-11','FISC','PC','2018-07-26',1),(30,'TP','Implementación de un sistema de cualquier cosa','1-123-11','FISC','PC','2018-08-21',3),(34,'P','Angular: Empty Fiddle','1-123-11','FISC','PC','2018-08-21',5),(36,'T','AngularJS Session Storage Example','1-123-11','FISC','PC','2018-08-21',6),(37,'P','AngularAgility sample','1-123-11','FISC','PC','2018-08-21',1),(38,'P','AngularJS Session Storage Example','1-123-11','FISC','PC','2018-08-21',1),(39,'P','Angular: Empty Fiddle','1-123-11','FISC','PC','2018-08-21',1),(40,'P','Angular: Empty Fiddle','1-123-11','FISC','PC','2018-08-21',6);
+INSERT INTO `graduation_works` VALUES (13,'P','Impresión automática de actas en el Tribunal Electoral para las eleciones','','5-482-691','FISC','PC','2018-07-13',2),(15,'TP','Diseño eléctrico realizado en la compañía ingeniería atlántico','','8-475-1024','FIE','PC','2017-10-08',2),(16,'P','Reconocimiento, evaluación y manejo de equipos especiales de fuerza en las centrales telefónicas.','','PE-14-5298','FII','PC','2017-06-10',3),(17,'TP','Sistema de gestión de la información del departamento de Bienestar Estudiantil de la Universidad Tecnológica de Panamá Centro Regional de Chiriquí','','5-482-691','FIE','PC','2018-02-10',1),(18,'P','Práctica profesional en Ricardo Perez S.A. \'Ahorro energético y mejoras en la calidad de la energía\'','','8-475-1024','FIE','PC','2018-02-10',2),(20,'T','Diseño y construcción del sistema didáctico SD-88 basado en el microprocesador INTEL8088','','PE-14-5298','FISC','PC','2018-07-26',4),(21,'T','Nuevo método computacional para el estudio de flujo de potenciaaa','','8-475-1024','FISC','Pc','2018-07-26',2),(22,'P','Mantenimiento preventivo en los sistemas de transmisión digital por cables de fibras óptica y microondas.','','5-482-691','FISC','PC','2018-07-26',3),(23,'P','Implementación de un sistema de información para la logística de materiales de una empresa industrial','','1-123-11','FISC','PC','2018-07-26',1),(30,'TP','Implementación de un sistema de cualquier cosa','','1-123-11','FISC','PC','2018-08-21',3),(36,'T','AngularJS Session Storage Example','','1-123-11','FISC','PC','2018-08-21',6),(37,'P','AngularAgility sample','','1-123-11','FISC','PC','2018-08-21',1),(44,'T','Esta es la tercera página','','1-123-11','FISC','PC','2019-02-19',1),(45,'T','Esta es la tercera página','','1-123-11','FISC','PC','2019-02-19',1),(47,'P','qqadad','','1-123-11','FISC','PC','2019-02-19',2),(48,'P','Otra prueba de este asunto','','1-123-11','FISC','PC','2019-02-19',1),(49,'P','Otra prueba de este asunto','','1-123-11','FISC','PC','2019-02-19',1),(50,'P','Otra prueba de este s','','1-123-11','FISC','PC','2019-02-19',1),(51,'P','Otra prueba de este saaaassss','','1-123-11','FISC','PC','2019-02-19',1),(52,'P','Otra prueba de este saaaasssdddss','','1-123-11','FISC','PC','2019-02-19',1),(56,'P','Esta es la tercera página','sssssssss','1-123-11','FISC','PC','2019-02-19',1),(57,'T','trabajo de prueba de recuperacion','sfafafgagag','1-123-11','FISC','PC','2019-02-19',3),(58,'P','trabajo de prueba actualizacion','hwrhr yjkedujsrt artjaryja','1-123-11','FISC','PC','2019-02-19',2),(59,'T','Otra prueba de este asunto','wetgwerte 4tyeryqerywyrtw','1-123-11','FISC','PC','2019-02-19',1),(60,'P','Esta es la tercera página4851','6646546816516841','1-123-11','FISC','PC','2019-02-21',1);
 /*!40000 ALTER TABLE `graduation_works` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +179,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `insertWork`(
 IN userId VARCHAR(45), 
 IN workTitle VARCHAR(200),
 IN workType CHAR(2),
-IN studentCount INT
+IN workDesc VARCHAR(255),
+IN studentCount INT,
+IN faculty1 VARCHAR(8),
+IN career1 VARCHAR(8),
+IN faculty2 VARCHAR(8),
+IN career2 VARCHAR(8),
+IN faculty3 VARCHAR(8),
+IN career3 VARCHAR(8)
 )
 BEGIN
 SELECT 
@@ -160,12 +196,31 @@ INTO
 FROM
     utp_tesis.utp_users
 WHERE
-	us_doc_num = '1-123-11';
+	us_doc_num = userId COLLATE utf8_general_ci;
     
 SELECT CURDATE() INTO @today;
 
-INSERT INTO graduation_works (work_type, w_title, advisor, faculty, reg_center, creation_date, studentsQty) 
-VALUES (workType, workTitle,userId, @facu, @center, CURDATE(), studentCount);
+INSERT INTO graduation_works (work_type, w_title, work_desc, advisor, faculty, reg_center, creation_date, studentsQty) 
+VALUES (workType, workTitle,workDesc,userId, @facu, @center, CURDATE(), studentCount);
+
+SELECT LAST_INSERT_ID() INTO @workCode;
+
+INSERT INTO candidates (work_code,faculty,career)
+VALUES (@workCode, faculty1, career1);
+
+IF STRCMP(faculty2,'') != 0 THEN
+
+INSERT INTO candidates (work_code,faculty,career)
+VALUES (@workCode, faculty2, career2);
+
+END IF;
+
+IF STRCMP(faculty3,'') != 0 THEN
+
+INSERT INTO candidates (work_code,faculty,career)
+VALUES (@workCode, faculty3, career3);
+
+END IF;
 
 END ;;
 DELIMITER ;
@@ -183,4 +238,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-07 16:51:24
+-- Dump completed on 2019-02-21 11:46:08
--- docs/scrips/Insert_utp_catalogs_items.sql
+++ docs/scrips/Insert_utp_catalogs_items.sql
@@ -92,3 +92,182 @@ VALUES("UF","Facultad de Ingeniería Mecánica","FIM","");
 
 INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
 VALUES("UF","Facultad de Ciencias y Tecnología","FCT","");
+
+
+-- -------- --
+-- CARRERAS --
+-- -------- --
+
+-- FISC --
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería de Sistemas y Computación","ISC","FISC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería de Sistemas de Información","ISI","FISC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería de Software","IS","FISC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Desarrollo de Software","LDS","FISC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Informática Aplicada a la Educación","LIAE","FISC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Redes Informáticas","LRI","FISC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Informática para la Gestión Empresarial","TIGE","FISC");
+
+-- FCT --
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Comunicación Ejecutiva Bilingüe","LCEB","FCT");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Forestal","IF","FCT");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería en Alimentos","IA","FCT");
+
+-- FIC --
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Civil","IC","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Topografía","LT","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Edificaciones","LE","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura En Dibujo Automatizado","LDA","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Geomática","IG","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Marítima Portuaria","IMP","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Ambiental","IAM","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Saneamiento y Ambiente","LSA","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Operaciones Marítimas y Portuarias","LOMP","FIC");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Geológica","IGE","FIC");
+
+-- FIE --
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería de Control y Automatización","ICA","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Eléctrica","IE","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Eléctrica y Electrónica","IEE","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Electromecánica","IEM","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Electrónica y Telecomunicaciones","IET","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería en Telecomunicaciones","IT","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Electrónica y Sistemas de Comunicación","LESC","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Sistemas Eléctricos y Automatización","LSEA","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Autotrónica","TAT","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Sistemas Eléctricos","TSE","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Telecomunicaciones","TTC","FIE");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Electrónica Biomédica","TEB","FIE");
+
+-- FII --
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Industrial","II","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Gestión Administrativa","LGA","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Mecánica Industrial","IMI","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Logística y Transporte Multimodal","LLTM","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Gestión de la Producción Industrial","LGPI","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Mercadeo y Comercio Internacional","LMCI","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Recursos Humanos y Gestión de la Productividad","LRHGP","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Logística y Cadena de Suministro","ILCS","FII");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Mercadeo y Negocios Internacionales","LMNI","FII");
+
+-- FIM --
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Mecánica","IM","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería de Mantenimiento","IMM","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Naval","IN","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería Aeronáutica","IAN","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Ingeniería de Energía y Ambiente","IEA","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Mecánica Industrial","LMI","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Refrigeración y Aire Acondicionado","LRAA","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Mecánica Automotriz","LMA","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Soldadura","LS","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Administración de Aviación con Opción a Vuelo (Piloto)","LAAOV","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Licenciatura en Administración de Aviación (Piloto)","LAA","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Despacho de Vuelo","TDV","FIM");
+
+INSERT INTO catalogs_items(cat_code, cati_name, cati_code, cati_code_rel)
+VALUES("UC","Técnico en Ingeniería de Mantenimiento de Aeronaves con especialización en Motores y Fuselaje","TIMAEMF","FIM");
