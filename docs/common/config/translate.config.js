1c952ec8d081d089f01bf6f759e5c4fff5f108be
Jorge Girón <32642460+kyoshi19@users.noreply.github.com>
2019-03-23 12:33:55 -0500

    Add files via upload

--- docs/common/config/translate.config.js
+++ docs/common/config/translate.config.js
@@ -12,19 +12,39 @@ var EStranslate = {
   'utp.name'                        : 'Universidad Tecnológica de Panamá',
   'header.faculty'                  : 'Facultad de Ingeniería de Sistemas Computacionales',
   'global.firstName'                : 'Nombre',
+  'global.faculty'                  : 'Facultad',
+  'global.carrer'                   : 'Carrera',
   'global.lastName'                 : 'Apellido',
-  'global.id'                       : 'Identificación',
+  'global.id'                       : 'Cédula',
+  'global.passport'                 : 'Pasaporte',
   'global.password'                 : 'Contraseña',
   'global.login'                    : 'Iniciar Sesión',
-  'global.main.student.page.title'  : 'Busca tu opción de trabajo de graduación',
-  'global.main.advisor.page.title'  : 'Administrar opciones de trabajo de graduación',
+  'global.cancel'                   : 'Cancelar',
+  'global.advisor'                  : 'Asesor',
+  'global.title'                    : 'Título',
   'global.email'                    : 'Correo electrónico',
   'global.subject'                  : 'Asunto',
   'global.message'                  : 'Mensaje',
   'global.documentation'            : 'Documentación',
-  'global.work.types'               : 'Tipos de trabajos',
   'global.main.rules'               : 'Normas principales',
+  'global.regional.center'          : 'Centro regional',
+  'global.key.words'                : 'Palabras claves',
+  'global.search.results'           : 'Resultados de la búsqueda',
+  'global.results.count'            : 'La búsqueda ha dado {{count}} resultado/s',
 
+  // TRABAJOS DE GRADUACIÓN
+  'global.work'                     : 'Trabajo de graduación',
+  'global.works'                    : 'Trabajos de graduación',
+  'global.my.works'                 : 'Mis trabajos de graduación',
+  'global.work.description'         : 'Descripción del trabajo',
+  'global.work.type'                : 'Tipo de trabajo',
+  'global.work.types'               : 'Tipos de trabajos',
+  'global.work.students.count'      : 'Cantidad de estudiantes',
+  'global.main.student.page.title'  : 'Busca tu opción de trabajo de graduación',
+  'global.main.advisor.page.title'  : 'Administrar opciones de trabajo de graduación',
+  'global.work.add'                 : 'Agregar trabajos de graduación',
+  'global.work.add.button'          : 'Agregar trabajo',
+  
   // MENSAJES DE ERROR DE VALIDACION
   'validation.required'             : 'El campo {0} es requerido.',
   'validation.mail'                 : "El campo {0} debe ser un correo electrónico.",
@@ -45,7 +65,7 @@ var EStranslate = {
   'footer.utp.phone'                : 'Teléfono. (507) 560-3000',
 
   /*************************
-  ****  NOTICIFACIONES  ****
+  ***   NOTICIFACIONES   ***
   *************************/
 
   'global.succes.work.added'        : 'Trabajo agregado correctamente',
