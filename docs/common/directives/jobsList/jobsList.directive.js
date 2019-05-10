0545939d324809aab1fcc3f9cfc511d0b30106a6
Jorge Girón <32642460+kyoshi19@users.noreply.github.com>
2019-05-10 16:26:02 -0500

    Add files via upload

--- docs/common/directives/jobsList/jobsList.directive.js
+++ docs/common/directives/jobsList/jobsList.directive.js
@@ -2,121 +2,106 @@
   'use strict';
 
   //  jobsList directive
-  function jobsList($log, isEmpty, storage, ModalService, NgTableParams, $http,
-  translate, notificationService) {
+  function jobsList($log, isEmpty, storage, ModalService, notificationService, mdDialog) {
     var directive = {
-      restrict        : 'E',
-      templateUrl     : 'common/directives/jobsList/jobsList.html',
-      scope           : {
-        works         : "=",
-        worksCount    : "=",
-        tableTitle    : "=",
-        userType      : "=",
-        showOptions   : "=",
-        searchWorks   : "&"
+      restrict: 'E',
+      templateUrl: 'common/directives/jobsList/jobsList.html',
+      scope: {
+        works: "=",
+        worksCount: "=",
+        tableTitle: "=",
+        userType: "=",
+        showOptions: "=",
+        searchWorks: "&"
       },
-      link            : linkFunc
+      link: linkFunc
     };
     return directive;
 
-    ////////
 
-    function linkFunc(scope, el, attr, ctrl) {
+    function linkFunc(scope) {
 
       /* --> VARIABLES <-- */
-
+      scope.selected = [];
       scope.user = storage.user;
 
       /* --> METODOS <-- */
 
-      scope.$watch('works', function(newValue, oldValue){
+      scope.$watch('works', function(newValue, oldValue) {
         resetTable();
       });
 
-      function worksTableConfig (){
-        var initialParams = {
-          count: 5 // filas por página
-        };
+      function worksTableConfig() {
+
         var initialSettings = {
-          // número de filas por botones de página
-          counts: null,
-          // determina los botones del paginador
-          paginationMaxBlocks: 7, //máximo de bloques de página
-          paginationMinBlocks: 1, //mínimo de bloques de página
-          //Información a mostrar
-          dataset: scope.works
+          limit: 3, // filas por página
+          page: 1, //Página actual
+          totalPages: scope.works.length, //Total de páginas
+          dataset: scope.works //Información a mostrar
         };
-        return new NgTableParams(initialParams, initialSettings);
+        return initialSettings;
       }
 
-      scope.openWorkInfo = function(work){
 
-        $log.debug('-->>PRUEBA MODAL<<--');
-        //Fuente --> https://www.npmjs.com/package/angular-modal-service
-
-        // Solo proporciona una url de plantilla, un controlador y llama a 'showModal'.
-        ModalService.showModal({
-          templateUrl: "common/templates/modal/choseWorkModal.html",
-          controller: "workController",
-          controllerAs:"ctrl",
-          inputs:{
-            data:work,
-            isEditing: false
-          }
-        }).then(function(modal) {
-          // El objeto modal tiene el elemento creado, si esto es un modal de bootstrap
-          // puedes llamar a 'modal' para mostrarlo, si se trata de un modal personalizado
-          // solo muestra (show) u oculta (hide) como se necesites.
-          modal.element.modal();
-          modal.close.then(function(response) {
-            $log.debug("Modal is closed ==>", response);
-            if (response.result) {
-              openEmailFormModal(work);
+      scope.openWorkInfo = function(work, event) {
+        mdDialog.show({
+            templateUrl: "common/templates/modal/choseWorkModal.html",
+            controller: "workController",
+            controllerAs: "ctrl",
+            clickOutsideToClose: true,
+            escapeToClose: true,
+            targetEvent: event,
+            locals: {
+              data: work,
+              isEditing: false
             }
-          });
-        });
-      };
 
-      var openEmailFormModal = function(work){
-
-        ModalService.showModal({
-          templateUrl: "common/templates/modal/sendEmailModal.html",
-          controller: "sendEmailController",
-          controllerAs:"ctrl",
-          backdrop:'static',
-          inputs:{
-            data:work,
-            isEditing: false
-          }
-        }).then(function(modal) {
-          modal.element.modal({
-            backdrop: 'static',
-            keyboard: false
+          })
+          .then(function(response) {
+            $log.info("My response-->", response);
+            openEmailFormModal(work);
+          }).catch(function(response) {
+            $log.error("My response-->", response);
           });
-          modal.close.then(function(response) {
+      }
+
+      var openEmailFormModal = function(work) {
+
+        mdDialog.show({
+            templateUrl: "common/templates/modal/sendEmailModal.html",
+            controller: "sendEmailController",
+            controllerAs: "ctrl",
+            clickOutsideToClose: true,
+            escapeToClose: true,
+            targetEvent: event,
+            locals: {
+              data: work,
+              isEditing: false
+            }
+
+          })
+          .then(function(response) {
             storage.showLoader = false;
-            if (response.result){
+            if (response.result) {
               if (isEmpty(response.data)) {
                 notificationService.showError('global.error.internet.conection');
                 return;
               }
               notificationService.showSucces('global.succes.mail.success');
             }
+          }).catch(function(err) {
+            $log.debug('ERROR ==> ', err);
           });
-          modal.close.catch(function(err){
-            $log.debug('ERROR ==> ',err);
-          });
-        });
       };
 
-      scope.openDeleteWork = function(work){
+      scope.openDeleteWork = function(work) {
 
         ModalService.showModal({
           templateUrl: "common/templates/modal/deleteWorkModal.html",
           controller: "workController",
-          controllerAs:"ctrl",
-          inputs:{
-            data:work,
+          controllerAs: "ctrl",
+          inputs: {
+            data: work,
             isEditing: false
 
           }
@@ -128,7 +113,7 @@
 
             if (response.data.error) {
               notificationService.showErrorT(response.data.error);
-            }else{
+            } else {
               if (response.data.records[0] > 0) {
                 notificationService.showSucces('global.succes.work.deleted');
                 scope.searchWorks();
@@ -144,14 +129,14 @@
 
       };
 
-      scope.openUpdateWorkModal = function(work){
+      scope.openUpdateWorkModal = function(work) {
         ModalService.showModal({
           templateUrl: "common/templates/modal/updateWorkModal.html",
           controller: "workController",
-          controllerAs:"ctrl",
-          inputs:{
-            data:work,
-            isEditing:true
+          controllerAs: "ctrl",
+          inputs: {
+            data: work,
+            isEditing: true
           }
         }).then(function(modal) {
 
@@ -177,8 +162,10 @@
         });
       };
 
-      function resetTable(){
-        scope.dataTable = worksTableConfig();
+      function resetTable() {
+        if (scope.works) {
+          scope.dataTable = worksTableConfig();
+        }
       }
     }
   }
@@ -188,15 +175,13 @@
     'isEmptyFilter',
     '$sessionStorage',
     'ModalService',
-    'NgTableParams',
-    '$http',
-    '$translate',
-    'notificationService'
+    'notificationService',
+    '$mdDialog'
   ];
 
   //  Module
   win.MainApp.Directives
-  .directive('jobsList', jobsList);
+    .directive('jobsList', jobsList);
 
 
 
